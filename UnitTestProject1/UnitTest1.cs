dda2cddaee823040956a113eb7592cdc32e6163b
Stanley Shannon <ssshannon1026@gmail.com>
2017-04-24 12:55:12 -0400

    github

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -19,7 +19,7 @@ namespace UnitTestProject1
         public void TestMethod2()
         {
             string path = sayHello.GetConfigurationPath();
-            int i = path.CompareTo("..\\..\\..\\..\\helloCrowe\\helloCrowe\\bin\\Debug\\helloCrowe.exe");
+            int i = path.CompareTo("..\\..\\..\\helloCrowe\\bin\\Debug\\helloCrowe.exe");
             Assert.AreEqual(0, i);
         }
     }
